5f86ceed4d7a7eff14de44ced64492da63fe5b29
tan <buitan561993@gmail.com>
2023-05-26 09:17:24 +0700

    update

--- Bai5_BT_TH/BT_01/Upload_music/src/main/java/ra/model/service/fileUpload/IFileUploadService.java
+++
@@ -1,7 +0,0 @@
-package ra.model.service.fileUpload;
-
-import ra.model.entity.FileUpload;
-import ra.model.service.IService;
-
-public interface IFileUploadService extends IService<FileUpload> {
-}
